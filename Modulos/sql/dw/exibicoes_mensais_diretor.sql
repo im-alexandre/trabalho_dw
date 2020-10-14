61677e85fcacdca67a6dc22ad2e7badc2f938cec
im-alexandre <48188379+im-alexandre@users.noreply.github.com>
2020-10-13 23:39:46 -0300

    Create exibicoes_mensais_diretor.sql

---
+++ Modulos/sql/dw/exibicoes_mensais_diretor.sql
@@ -0,0 +1,6 @@
+SELECT p.nome_artista, t.mes, count(e.id_exibt) exibicoes
+from fato_exibt e
+join dim_particip p on e.cod_particip=p.id_particip
+join dim_tempo t on e.cod_data=t.id_tempo
+where p.papel='Diretor'
+group by p.nome_artista, p.papel, t.mes
