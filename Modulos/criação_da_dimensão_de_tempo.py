c62f3316666eb62e0e4c7858d97a84435c855260
im-alexandre <48188379+im-alexandre@users.noreply.github.com>
2020-10-13 23:48:14 -0300

    Update criação_da_dimensão_de_tempo.py

--- Modulos/criação_da_dimensão_de_tempo.py
+++ Modulos/criação_da_dimensão_de_tempo.py
@@ -5,6 +5,7 @@ Script para a criação da dimensão tempo
 
 import locale
 from datetime import datetime
+import holidays # Incluí o import para tratar feriados. (pip install holidays), caso não tenha o pcte instalado.
 
 import numpy as np
 import pandas as pd
@@ -18,10 +19,16 @@ data_home = '/home/alexandre/Cursos/PUC/datawarehousing/lab1/Dados'
 # definindo a linguagem para PT_BR
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 
+# criando uma lista com os feriados no Brasil no período:
+feriados_bra= holidays.Brazil()
+feriados = []
+for feriado in feriados_bra['2002-12-31': '2003-12-31'] :
+    feriados.append(feriado)
+
 # Criando dataframe vazio para rececber os dados de tempo
 df = pd.DataFrame()
 # utilizando o método pandas.date_range para gerar um iterável de datas e
-# O END É EXCLUSIVO, OU SEJA, SE FOR INFORMADA A ÚLTIMA DATA PRESENTE NO 
+# O END É EXCLUSIVO, OU SEJA, SE FOR INFORMADA A ÚLTIMA DATA PRESENTE NO
 # DATASET, PERDEM-SE REGISTROS
 df['data'] = pd.date_range(start='31/12/2002', end='31/12/2003')
 """redefinindo a coluna para ser o nome de data
@@ -32,12 +39,15 @@ df['mes_abreviado'] = df['data'].dt.month_name(locale="pt_BR.UTF-8")
 df['dia'] = df['data'].dt.day
 df['mes'] = df['data'].dt.month
 df['ano'] = df['data'].dt.year
+df['semana'] = df['data'].apply(lambda x: datetime.date(x).isocalendar()[1])
 df['dia_da_semana'] = df['data'].dt.dayofweek
 df['dia_da_semana_nome'] = df['data'].dt.day_name(locale="pt_BR.UTF-8")
 df['dia_do_ano'] = df['data'].dt.dayofyear
 df['trimestre'] = df['data'].dt.quarter
 df['semestre'] = np.where(df['trimestre'].isin([1, 2]), 1, 2)
 df['final_de_semana'] = np.where(df['dia_da_semana'].isin([5, 6]), 1, 0)
+df['feriado'] = df['data'].apply(lambda x: 1 if x in feriados else 0)
+df['dia_util'] = np.where((df['final_de_semana'] + df['feriado']) == 0, 1, 0)
 df['data_completa'] = df['dia_da_semana_nome'] + ", " + df['dia'].astype(
     str) + " de " + df['mes_nome'] + " de " + df['ano'].astype(str)
 
